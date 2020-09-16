1d248993e4bccbe7643a2ffd6f8d5ebec9d7492a
Stephen <stephen_h369@live.co.uk>
2020-09-16 10:48:06 +0100

    initial commit, working beep

---
+++ src/containers/MetronomeContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Metronome from '../components/Metronome';
+
+const MetronomeContainer = () => {
+  return (
+    <Metronome/>
+  );
+}
+
+export default MetronomeContainer;
